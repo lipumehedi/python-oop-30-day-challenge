362536a567d26bada35f77bc8a96bb885e1ffa76
lipumehedi <mehedilipu@gmail.com>
2026-09-19 17:19:56 +0900

    Fix project folder structure

--- python-oop-30-day-challenge/Day01_Class_object/practice.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    pass
-
-
-student1 = Student()
-student2 = Student()
-
-print(student1)
-print(student2)
--- python-oop-30-day-challenge/README.md
+++
@@ -1,2 +0,0 @@
-# python-oop-30-day-challenge
-30-day Python OOP and problem-solving challenge
